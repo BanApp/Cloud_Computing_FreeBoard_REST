46a2ae6ffefcb2214636d5353169e152961872be
BanApp <jmj284@gmail.com>
2023-03-21 01:13:05 +0900

    03/21

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class BoardController {
@@ -16,16 +18,20 @@ public class BoardController {
     private BoardService boardService;
 
     @GetMapping("/board/write")
-    public String boardWriteForm(){
+    public String boardWriteForm()
+    {
         return "boardwrite";
     }
 
     @PostMapping("/board/writepro")
-    public String boardWritePro(Board board)
+    public String boardWritePro(Board board, Model model, Integer id, MultipartFile file) throws Exception
     {
-        boardService.write(board);
+        boardService.write(board, file);
 
-        return "";
+        model.addAttribute("message","글작성이 완료되었습니다!");
+        model.addAttribute("searchUrl","/board/view?id=" + board.getId());
+        //model.addAttribute("searchUrl","/board/list");
+        return "message";
     }
 
     @GetMapping("board/list")
@@ -42,10 +48,33 @@ public class BoardController {
     }
 
     @GetMapping("board/delete") //localhost:8080/board/view?id=1
-    public String boardDelete(Integer id){
+    public String boardDelete(Integer id,Model model){
         boardService.boardDelete(id);
+
+        model.addAttribute("message","삭제완료!");
+        model.addAttribute("searchUrl","/board/list");
+
+
+        return "message";
+    }
+
+    @GetMapping("board/modify/{id}") //localhost:8080/board/view?id=1
+    public String boardModify(@PathVariable("id") Integer id, Model model){
+        model.addAttribute("board",boardService.boardView(id));
+
+        return "boardmodify";
+    }
+
+    @PostMapping("board/update/{id}") //localhost:8080/board/view?id=1
+    public String boardUpdate(@PathVariable("id") Integer id, Board board, MultipartFile file) throws Exception
+    {
+
+        Board boardTemp = boardService.boardView(id);
+        boardTemp.setTitle(board.getTitle());
+        boardTemp.setContent(board.getContent());
+
+        boardService.write(boardTemp,file);
 
         return "redirect:/board/list";
     }
-
 }
