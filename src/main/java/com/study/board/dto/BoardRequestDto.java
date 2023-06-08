bad179ae01525e144bf5b4e839dbfb94f998a42d
홍찬희 <ghdcksgml2@naver.com>
2023-06-08 13:19:54 +0900

    add: DTO추가 및 Cors 설정

---
+++ src/main/java/com/study/board/dto/BoardRequestDto.java
@@ -0,0 +1,12 @@
+package com.study.board.dto;
+
+import lombok.Data;
+
+@Data
+public class BoardRequestDto {
+    private String title;
+    private String content;
+    private String nickname;
+    private String company;
+    private long user_id;
+}
